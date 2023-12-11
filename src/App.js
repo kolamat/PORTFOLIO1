6ca4cf9a627960164b6d80e82d880d9f19c15e31
kolamat <matanmidupe004@gmail.com>
2023-12-11 02:37:43 +0100

    On branch main

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Hero from "./components/hero";
 import About from "./components/About";
